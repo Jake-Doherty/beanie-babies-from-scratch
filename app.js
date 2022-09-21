bdd0b3f42afa2a709e3e8f07df18cf736bf5b5a8
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-09-21 16:43:10 -0700

    display of beanie babies works but displays in single column

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 /* Imports */
+import { getBeanies } from './fetch-utils.js';
+import { renderBeanie } from './render-beanies.js';
 
 /* Get DOM Elements */
+const beanieList = document.getElementById('beanie-list');
 
 /* State */
+let error = null;
+let beanies = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    findBeanies();
+});
+
+async function findBeanies(title, releaseYear) {
+    const response = await getBeanies(title, releaseYear);
+
+    error = response.error;
+    beanies = response.data;
+
+    if (!error) {
+        displayBeanies();
+    }
+}
 
 /* Display Functions */
 
+function displayBeanies() {
+    beanieList.innerHTML = '';
+
+    for (const beanie of beanies) {
+        const beanieElement = renderBeanie(beanie);
+        beanieList.append(beanieElement);
+    }
+}
+
 // (don't forget to call any display functions you want to run on page load!)
+displayBeanies();
---
+++ render-beanies.js
@@ -0,0 +1,31 @@
+export function renderBeanie(beanie) {
+    const li = document.createElement('li');
+    li.classList.add('beanie-card');
+
+    const beanieInfoDiv = document.createElement('div');
+    beanieInfoDiv.classList.add('info-card');
+
+    const h2 = document.createElement('h2');
+    h2.textContent = beanie.title;
+
+    const attributes = document.createElement('p');
+    attributes.classList.add('attr');
+
+    const animal = document.createElement('span');
+    animal.textContent = beanie.animal;
+
+    const astrologySign = document.createElement('span');
+    astrologySign.textContent = beanie.astrologySign;
+
+    const beanieImage = document.createElement('img');
+    beanieImage.src = beanie.image;
+    beanieImage.alt = beanie.title;
+
+    attributes.append(animal, astrologySign);
+
+    beanieInfoDiv.append(h2, attributes);
+
+    li.append(beanieInfoDiv, beanieImage);
+
+    return li;
+}
